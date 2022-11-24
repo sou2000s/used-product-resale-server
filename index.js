d45637f667a0ea14757633b6c48800f4262242dc
netox222 <souparnamahato146@gmail.com>
2022-11-24 20:12:21 +0530

    delete buyrs api created

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.put('/users' , async(req , res)=>{
         const user = req.body
     const result = await usersCollection.insertOne(user)
     res.send(result)
-    console.log(result);
+   
      } catch (error) {
         console.log(error.message);
      }
@@ -60,7 +60,7 @@ app.get('/products/:categoryName' , async(req , res)=>{
         const categoryName = (req.params.categoryName.toLowerCase());
     const query = {categoryName: categoryName}
     const products = await productsCollection.find(query).toArray()
-    console.log(products);
+    
     res.send(products)
     } catch (error) {
         console.log(error.message);
@@ -106,13 +106,44 @@ app.get('/users/allSellers' , async(req , res)=>{
     const query = {}
     const users = await usersCollection.find(query).toArray()
     const allSellers =  users.filter(user => user.role === "Seller Accout")
-    console.log(allSellers);
+    
     res.send(allSellers)
    } catch (error) {
     console.log(error.message);
    }
     
 })
+
+//  verify a seller
+app.put('/users/allsellers/verified/:id' , async(req , res)=>{
+    try {
+        const id = req.params.id
+    const filter = {_id: ObjectId(id)}
+    const options = {upsert: true}
+    const updatedDoc = {
+        $set: {
+            verified: "verified"
+        }
+
+    }
+    const result = await usersCollection.updateOne(filter , updatedDoc , options)
+     
+    res.send(result)
+    } catch (error) {
+        console.log(error.message);
+    }
+})
+
+////////////////////
+
+app.get('/sellers' , async(req , res)=>{
+    const sellers = await usersCollection.findOne({email: req.query.email})
+    res.send(sellers)
+})
+
+
+
+//  //////////////
 app.get('/users/allBuyrs' , async(req , res)=>{
    try {
     const query = {}
@@ -128,6 +159,20 @@ app.get('/users/allBuyrs' , async(req , res)=>{
 })
 
 
+// sellers products 
+
+app.get('/sellers/products' , async(req , res)=>{
+    try {
+        const email = req.query.email;
+    const query = {sellerEmail:email}
+    const sellerSpecificProducts = await productsCollection.find(query).toArray()
+   
+    res.send(sellerSpecificProducts)
+    } catch (error) {
+        console.log(error.message);
+    }
+})
+
 app.delete('/users/allSellers/:id' , async(req , res)=>{
     try {
     const  id = req.params.id
@@ -138,6 +183,20 @@ app.delete('/users/allSellers/:id' , async(req , res)=>{
          console.log(error.message);
     }
 })
+
+
+app.delete('/users/allBuyrs/:id' , async(req , res)=>{
+    try {
+    const  id = req.params.id
+    const query = {_id: ObjectId(id)}
+    const deletedSeller = await usersCollection.deleteOne(query)
+    res.send(deletedSeller)
+    } catch (error) {
+         console.log(error.message);
+    }
+})
+
+
 
 
 
