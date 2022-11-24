05791b3f3d9e9d2c6f4500d5f8c699cd6ed84cd2
netox222 <souparnamahato146@gmail.com>
2022-11-24 09:35:45 +0530

    category api adeded

--- index.js
+++ index.js
@@ -13,11 +13,14 @@ app.use(cors())
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://useProductResale:wP5x0JnhX5NWNig7@cluster0.x7kxg5y.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.x7kxg5y.mongodb.net/?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 const usersCollection = client.db('UsedProductDatabase').collection('users')
+const productsCollection = client.db('UsedProductDatabase').collection('products')
+const categoriesCollection = client.db('UsedProductDatabase').collection('categories')
 
 const dbConnect = async ()=>{
     try {
@@ -49,6 +52,30 @@ app.get('/users/role/:email' , async(req , res)=>{
     } catch (error) {
         
     }
+})
+
+
+app.get('/products/:categoryName' , async(req , res)=>{
+    try {
+        const categoryName = (req.params.categoryName.toLowerCase());
+    const query = {categoryName: categoryName}
+    const products = await productsCollection.find(query).toArray()
+    console.log(products);
+    res.send(products)
+    } catch (error) {
+        console.log(error.message);
+    }
+})
+
+
+app.get('/categories' , async(req , res)=>{
+   try {
+    const query = {}
+    const categories = await categoriesCollection.find(query).toArray()
+    res.send(categories)
+   } catch (error) {
+    console.log(error.message);
+   }
 })
 
 
